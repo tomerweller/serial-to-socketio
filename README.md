0656a4f80848e20c345a2566587b11cc8eeebcf9
Tomer Weller <tomer.weller@gmail.com>
2014-11-11 17:56:22 -0500

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+#Serial to SocketIO
+This project demonstrates how to implement a thin proxy server in [node.js](http://nodejs.org/) that reads input from the serial port and forwards it to web clients via [socket.io](http://socket.io).
+
+
+The specific client used in this demo presents temperature readings from a board matching Neil Gershenfeld's [hello.temp.45](http://academy.cba.mit.edu/classes/input_devices/temp/hello.temp.45.png) example.
+
+##Usage
+### install prerequisites
+    #npm install
+### Run
+    #node app.js <serial_port>
+
+If everything is working you should be able to see temperature readings at [http://127.0.0.1:3000](http://127.0.0.1:3000).
---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var serialPort = require("serialport");
+
+if (process.argv.length != 3) {
+    console.log("usage: \"node app.js <serial_port>\"");
+    process.exit(0);
+}
+
+//setup expressjs to serve static files from the static directory and redirect root to index.html
+app.use("/", express.static(__dirname + "/static"));
+app.get("/", function (req, res) {
+    res.redirect('/index.html');
+});
+
+http.listen(3000, function () {
+    console.log('go to http://127.0.0.1:3000');
+});
+
+//initialize serial connection
+var serialConnection = new serialPort.SerialPort(process.argv[2], {
+    parser: serialPort.parsers.byteLength(1), //
+    baudrate: 9600
+});
+
+//on data callback broadcast to the default socketio connection
+serialConnection.on("open", function () {
+    serialConnection.on('data', function (data) {
+        io.emit("data", data[0]);
+    });
+});
+
+//error handling
+serialConnection.on("error", function () {
+    console.error("Can't establish serial connection with " + process.argv[2]);
+    process.exit(1);
+});
